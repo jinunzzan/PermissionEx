63f8a6a4ece52a3ad13321f7b792f1070d9be599
jinunzzan <lovebox6607@gmail.com>
2023-05-31 11:48:07 +0900

    코드 간소화

--- PermissionEx/ViewController.swift
+++ PermissionEx/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     
     //권한체크
     @IBAction func setPermission(_ sender: UIButton) {
+        //23.05.31 김은찬
         let authStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
         //최초 권한요청 시스템 알럿창
         if authStatus == .notDetermined {
@@ -33,19 +34,40 @@ class ViewController: UIViewController {
             print("권한 설정 허용 상태")
         } else {
             print("권한 설정 거부 상태")
-            let alert = UIAlertController(title: "설정에서 권한을 허용해야 합니다", message: "", preferredStyle: .alert)
-            let settingBtn = UIAlertAction(title: "설정하기", style: .default, handler: { _ in
-                if (UIApplication.shared.canOpenURL(URL(string: UIApplication.openSettingsURLString)!)){
-                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                }
-            })
-            alert.addAction(settingBtn)
-            let refuseBtn = UIAlertAction(title: "거절", style: .destructive)
-            alert.addAction(refuseBtn)
-            present(alert, animated: true, completion: nil)
-            print("alert 표시")
+            //alert 함수 사용해서 간소화
+            alert(title: "설정에서 권한을 허용해야 합니다", message: "", okTitle: "설정하기", okHandler: { _ in if (UIApplication.shared.canOpenURL(URL(string: UIApplication.openSettingsURLString)!)){UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)}}, cancelTitle: "거절", cancelHandler: { _ in print("거절버튼 클릭") })
+            
             
         }
+        
+        //        //최초 권한요청 시스템 알럿창
+        //        if authStatus == .notDetermined {
+        //            AVCaptureDevice.requestAccess(for: AVMediaType.video){ response in
+        //                if response {
+        //                    print("허용되었음")
+        //                } else {
+        //                    print("허용하지않음")
+        //                }
+        //            }
+        //        }
+        //        else if authStatus == AVAuthorizationStatus.authorized {
+        //            print("권한 설정 허용 상태")
+        //        } else {
+        //            print("권한 설정 거부 상태")
+        //            let alert = UIAlertController(title: "설정에서 권한을 허용해야 합니다", message: "", preferredStyle: .alert)
+        //            let settingBtn = UIAlertAction(title: "설정하기", style: .default, handler: { _ in
+        //                if (UIApplication.shared.canOpenURL(URL(string: UIApplication.openSettingsURLString)!)){
+        //                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        //                }
+        //            })
+        //            alert.addAction(settingBtn)
+        //            let refuseBtn = UIAlertAction(title: "거절", style: .destructive)
+        //            alert.addAction(refuseBtn)
+        //            present(alert, animated: true, completion: nil)
+        //            print("alert 표시")
+        //
+        //        }
+        //    }
+        
     }
-    
 }
