a1896502a358f2c749a04a37e304e00334edd68e
jinunzzan <lovebox6607@gmail.com>
2023-05-31 09:38:34 +0900

    Initial Commit

---
+++ PermissionEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PermissionEx
+//
+//  Created by Eunchan Kim on 2023/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
