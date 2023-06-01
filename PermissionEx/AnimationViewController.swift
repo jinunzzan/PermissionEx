f956509f3e38e6f33e445486801b0128d60e045c
jinunzzan <lovebox6607@gmail.com>
2023-06-01 16:47:40 +0900

    뷰 하나에서 애니메이션으로 성공!

--- PermissionEx/AnimationViewController.swift
+++ PermissionEx/AnimationViewController.swift
@@ -18,35 +18,48 @@ class AnimationViewController: UIViewController {
         super.viewDidLoad()
         bottomView.isHidden = true
        
-        buttonSet()
+       
     }
     override func viewDidAppear(_ animated: Bool) {
         
-        viewAnimation()
+        viewAppear()
+        
     }
-    func viewAnimation(){
+    func viewAppear(){
         bottomView.isHidden = false
         UIView.animate(withDuration: 0.5, delay: 0, animations: { () -> Void in
             self.bottomView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 350)
         },
                        completion: { (didFinish) -> Void in
+            
+            self.buttonSet()
         })
         
+    }
+    func viewDisppear(){
+        UIView.animate(withDuration: 0.1, delay: 0, animations: { [weak self] in
+            guard let self = self else { return }
+            let screenHeight = UIScreen.main.bounds.height
+            self.bottomView.frame = CGRect(x: 0, y: screenHeight, width: self.bottomView.frame.width, height: self.bottomView.frame.height)
+        }, completion: { (didFinish) -> Void in
+            self.bottomView.isHidden = true
+            self.dismiss(animated: false)
+        })
     }
     func buttonSet(){
         negativeButton.layer.borderColor = UIColor.systemGray5.cgColor
         negativeButton.layer.borderWidth = 1
         negativeButton.addAction(UIAction(handler: { _ in
-            UIView.animate(withDuration: 0.5, delay: 0, animations: { [weak self] () -> Void in
-                guard let self = self else { return }
-                self.bottomView.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 350, width: UIScreen.main.bounds.width, height: 350)
-            }, completion: { (didFinish) -> Void in
-                // 애니메이션이 완료되었을 때의 처리를 추가할 수 있습니다.
-                self.bottomView.isHidden = true
-            })
-//            self.dismiss(animated: false){
-//
-//            }
+//            UIView.animate(withDuration: 0.5, delay: 0, animations: { [weak self] () -> Void in
+//                guard let self = self else { return }
+//                self.bottomView.frame = CGRect(x: 0, y:0, width: UIScreen.main.bounds.width, height: 350)
+//            }, completion: { (didFinish) -> Void in
+//                // 애니메이션이 완료되었을 때의 처리를 추가할 수 있습니다.
+//                self.bottomView.isHidden = true
+//                self.dismiss(animated: false)
+//            })
+            self.viewDisppear()
+//            self.dismiss(animated: false)
         }), for: .touchUpInside)
     }
 
