76b5dbfa1550d289d45a623c3a3f49708a9978a9
jinunzzan <lovebox6607@gmail.com>
2023-06-02 13:48:36 +0900

    정리

--- PermissionEx/AnimationViewController.swift
+++ PermissionEx/AnimationViewController.swift
@@ -10,18 +10,14 @@ import UIKit
 class AnimationViewController: UIViewController {
     
     @IBOutlet weak var bottomView: UIView!
-    
     @IBOutlet weak var negativeButton: UIButton!
-    
     @IBOutlet weak var positiveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         bottomView.isHidden = true
-       
-       
     }
     override func viewDidAppear(_ animated: Bool) {
-        
         viewAppear()
         
     }
@@ -50,16 +46,7 @@ class AnimationViewController: UIViewController {
         negativeButton.layer.borderColor = UIColor.systemGray5.cgColor
         negativeButton.layer.borderWidth = 1
         negativeButton.addAction(UIAction(handler: { _ in
-//            UIView.animate(withDuration: 0.5, delay: 0, animations: { [weak self] () -> Void in
-//                guard let self = self else { return }
-//                self.bottomView.frame = CGRect(x: 0, y:0, width: UIScreen.main.bounds.width, height: 350)
-//            }, completion: { (didFinish) -> Void in
-//                // 애니메이션이 완료되었을 때의 처리를 추가할 수 있습니다.
-//                self.bottomView.isHidden = true
-//                self.dismiss(animated: false)
-//            })
             self.viewDisppear()
-//            self.dismiss(animated: false)
         }), for: .touchUpInside)
     }
 
